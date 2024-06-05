048c895fc0816e6260b493f5474ffd63cb8c94de
Trevor Pendras <trevor@pendras.com>
2024-06-05 16:45:50 -0700

    New renv snapshot

--- tidytuesday2024/tidy20/tidy20_exp.R
+++ tidytuesday2024/tidy20/tidy20_exp.R
@@ -1,12 +1,16 @@
 library(tidytuesdayR)
 library(tidyverse)
+library(caret)
+library(stringr)
+library(vcd)
 library(ggforce)
+source("one_hot_tt1.R")
 
 # Import data
 coffee_survey <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-05-14/coffee_survey.csv')
 
 # Trim out free-text data, fix "spend" typo ("spend" is not a noun, stop trying to make it one)
-clean_tib <- coffee_survey %>%
+trimdata <- coffee_survey %>%
   select(-matches("other|specify|coffee_a|coffee_b|coffee_c|coffee_d|prefer")) %>%
   rename(monthly_expenditure = total_spend) %>%
   mutate(across(everything(), ~ if(is.character(.)) tolower(.) else .)) %>%
@@ -15,8 +19,7 @@ clean_tib <- coffee_survey %>%
                   most_paid, most_willing, value_cafe, spent_equipment,
                   value_equipment, gender, education_level, ethnicity_race,
                   employment_status, number_children, political_affiliation
-                  ), as.factor
-                )
+                  ), as.factor)
          )
 
 # List of columns with multi-value cells
@@ -29,21 +32,49 @@ hot_columns <- c("where_drink",
                  "why_drink"
 )
 
-# Drop multi-value columns before dropping NA cells
-strip_tib <- clean_tib %>%
+trimdata_factors_clean <- trimdata %>%
   select(-all_of(hot_columns)) %>%
   drop_na()
 
-expertiseVknowsource_tib <- strip_tib %>%
+# Function to calculate Cramér's V
+cramers_v <- function(x, y) {
+  chi2 <- chisq.test(x, y)
+  n <- sum(chi2$observed)
+  phi2 <- chi2$statistic / n
+  r <- length(unique(x))
+  k <- length(unique(y))
+  return(sqrt(phi2 / min(r - 1, k - 1)))
+}
+
+# Remove submission id
+factor_columns <- select(trimdata_factors_clean, -submission_id)
+
+# Generate all combinations of pairs of factor columns
+combinations <- combn(names(factor_columns), 2, simplify = FALSE)
+
+# Calculate Cramér's V for each pair
+results <- map_dfr(combinations, function(cols) {
+  cramer_v <- cramers_v(factor_columns[[cols[1]]], factor_columns[[cols[2]]])
+  tibble(
+    var1 = cols[1],
+    var2 = cols[2],
+    cramers_v = cramer_v
+  )
+})
+
+# Rank the pairs by Cramér's V
+results <- results %>% arrange(desc(cramers_v))
+
+# No significant unexpected correlation found
+
+# Investigate expretise vs know source
+exp_source <- trimdata_factors_clean %>%
   select(expertise, know_source)
 
 set.seed(357)
 jitter <- position_jitter(width = 0.25, height = 0.5)
 
-p <- ggplot(expertiseVknowsource_tib, aes(
-  x = know_source, 
-  y = as.numeric(expertise), 
-  color = know_source)) +
+p <- ggplot(exp_source, aes(x = know_source, y = as.numeric(expertise), color = know_source)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.6) +
   geom_jitter(position = jitter, alpha = 0.2) +
   scale_y_continuous(breaks = seq(0, 10, by = 1)) +
@@ -54,30 +85,30 @@ p <- ggplot(expertiseVknowsource_tib, aes(
   ) +
   theme_minimal() +
   theme(legend.position = "none")
+  
+print(p)
 
-p
-
-set.seed(357)
-p_hilite <- p +
+p_hilite <- ggplot(exp_source, aes(x = know_source, y = as.numeric(expertise), color = know_source)) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.6) +
+  geom_jitter(position = jitter, alpha = 0.2) +
+  scale_y_continuous(breaks = seq(0, 10, by = 1)) +
+  labs(
+    title = "Distribution of Knowledge of Source by Self-Reported Expertise",
+    x = "Do You Know Where Your Coffee Comes From?",
+    y = "Claimed Level of Coffee Expertise"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none") +
   geom_mark_ellipse(aes(fill = know_source,
                         filter = (know_source == "no") & (expertise == 10),
-                        description = "Experts"),
-                    label.fill = NA,
-                    label.colour = "darkgrey",
-                    label.buffer = unit(0, "mm"),
-                    con.border = "all",
-                    con.colour = "darkgrey",
-                    con.cap = 0,
-                    position = jitter
-  )
+                        description = "Geniuses"),
+                    con.type = "none",
+                    position = jitter)
 
-p_hilite
+print(p_hilite)
 
-expVsource_outliers <- strip_tib %>%
+exp_source_outliers <- trimdata_factors_clean %>%
   mutate(expertise = as.integer(expertise)) %>%
-  filter(expertise == 10) %>%
+  filter(expertise > 9) %>%
   filter(str_detect(know_source, "no"))
 
-kable(
-  expVsource_outliers
-)
